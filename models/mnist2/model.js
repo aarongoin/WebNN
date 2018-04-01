1a49311620e7df69472d88eabf3f0708ccd76ffc
Ron Cotton <rcotton4@students.clark.edu>
2018-04-01 14:13:34 -0700

    changed model

--- models/mnist2/model.js
+++ models/mnist2/model.js
@@ -13,9 +13,15 @@
 //const IMAGE_SIZE = 784;
 //const NUM_CLASSES = 10;
 module.exports = [
-    { input: 784 },
-    /* YOUR CUSTOM LAYERS */
-    { output: 10, loss: 'mean_squared_error' }
+    { conv2d: { inputShape: [28, 28, 1], kernelSize: 5, filters: 8, strides: 1, activation: 'relu', kernelInitializer: 'varianceScaling'} },
+    { maxPooling2d: {poolSize: [2, 2], strides: [2, 2]} },
+    { conv2d: { kernelSize: 5, filters: 16, strides: 1, activation: 'relu', kernelInitializer: 'varianceScaling'} },
+    { maxPooling2d: {poolSize: [2, 2], strides: [2, 2]} },
+    { flatten: 'flatten' },
+    { dense: { units: 10, kernelInitializer: 'varianceScaling', activation: 'softmax' } },
+    { output: { optimizer: 'sgd', loss: 'categoricalCrossentropy', metrics: ['accuracy'] } }
+
+
 ]
 
 
